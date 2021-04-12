362dbdace6a41d564e98418899072879b8869d31
Kelly Morris <70837349+kmorris896@users.noreply.github.com>
2021-04-11 20:24:36 -0400

    Now adds to database.  lets see if we can do an update.

--- index.js
+++ index.js
@@ -38,8 +38,41 @@ client.on('guildMemberAdd', member => {
   logger.info("Member ID: " + member.id);
   logger.info("Joined at: " + member.joinedTimestamp);
 
+  let memberItem = {
+    TableName: "installations",
+    Key: {"serverId": member.guild.id},
+    UpdateExpression: "set joinList.#memberId = :memberMap",
+    ExpressionAttributeNames: { "#memberId": member.id },
+    ExpressionAttributeValues: { ":memberMap": {"joinDateTime": member.joinedTimestamp}}
+  }
+  
+  logger.info("Adding to DynamoDB...");
+  updateItem(memberItem);
+
 });
 
+
+
+async function updateItem(params) {
+  var AWS = require("aws-sdk");
+  console.log("Version: " + AWS.VERSION);
+
+  const awsDynamoDbEndpoint = "https://dynamodb." + process.env.AWS_REGION + ".amazonaws.com";
+  logger.debug("Setting endpoint: " + awsDynamoDbEndpoint);
+  // var awsConfig = new AWS.Config();
+  AWS.config.update({endpoint: awsDynamoDbEndpoint, region: process.env.AWS_REGION});
+  AWS.config.apiVersions = { dynamodb: '2012-08-10' };
+
+  // var dynamodb = new AWS.DynamoDB();
+  var docClient = new AWS.DynamoDB.DocumentClient();
+  docClient.update(params, function(err, data) {
+    if (err) {
+      console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+    }
+  });
+} 
 
 
 async function loadServerConfig() {
