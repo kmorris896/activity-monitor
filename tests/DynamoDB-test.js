78c4caf8e3e2e957c0d0b184ecc16766fe1c7f92
Kelly Morris <70837349+kmorris896@users.noreply.github.com>
2021-05-01 18:29:10 +0000

    Dynamo tests now work.

--- tests/DynamoDB-test.js
+++ tests/DynamoDB-test.js
@@ -13,13 +13,20 @@ const logger = winston.createLogger({
 logger.info("Creating AWS SDK Object");
 var AWS = require("aws-sdk");
 console.log("AWS SDK Version: " + AWS.VERSION);
+AWS.config.getCredentials(function(err) {
+  if(err) logger.error(err.stack);
+  else {
+    logger.info("Access key: " + AWS.config.credentials.accessKeyId);
+  }
+});
 const awsDynamoDbEndpoint = "https://dynamodb." + process.env.AWS_REGION + ".amazonaws.com";
 logger.debug("Setting endpoint: " + awsDynamoDbEndpoint);
+
 // var awsConfig = new AWS.Config();
 AWS.config.update({endpoint: awsDynamoDbEndpoint, region: process.env.AWS_REGION});
 AWS.config.apiVersions = { dynamodb: '2012-08-10' };
+AWS.config.logger = console;
 
-// var dynamodb = new AWS.DynamoDB();
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 
@@ -29,17 +36,16 @@ logger.info("Getting items in configTable_d8c7c4d5:")
 
 const configParams = {
   TableName: "configTable_d8c7c4d5",
-  KeyConditionExpression: "serverId = :sid",
-  expressionAttributeValues: {
-    ":sid": "704057794571272362"
+  Key: {
+    "serverId": "704057794571272362"
   }
 }
 
-docClient.query(configParams, function(err, data) {
+docClient.get(configParams, function(err, data) {
   if (err) {
-    logger.error("Unable to read item.  Error JSON: ", JSON.stringify(err, null, 2));
+    logger.error("Unable to get.  Error JSON: " + JSON.stringify(err, null, 2));
   } else {
-    logger.info("GetItem succeeded:", JSON.stringify(data, null, 2));
+    logger.info("get succeeded: " + JSON.stringify(data, null, 2));
   }
 });
 
@@ -55,68 +61,75 @@ docClient.query(configParams, function(err, data) {
 //   }
 // }
 
-// getItem(joinParams);
+// docClient.get(configParams, function(err, data) {
+//   if (err) {
+//     logger.error("Unable to get.  Error JSON: ", JSON.stringify(err, null, 2));
+//   } else {
+//     logger.info("get succeeded:", JSON.stringify(data, null, 2));
+//   }
+// });
 
 
 
 
-async function getItem(params) {
-  var docClient = await createAwsDynamoDBObject();
 
-  logger.debug("Getting from table: " + params.TableName);
-  logger.debug(JSON.stringify(params, null, 2));
-  docClient.get(params, function(err, data) {
-    if (err) {
-      logger.error("Unable to GET item. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-      logger.debug("getItem succeeded:", JSON.stringify(data, null, 2));
-      return data;
-    }
-  });
-} 
+// async function getItem(params) {
+//   var docClient = await createAwsDynamoDBObject();
 
-
-async function putItem(params) {
-  var docClient = await createAwsDynamoDBObject();
-
-  logger.debug("Putting into table: " + params.TableName);
-  logger.debug(JSON.stringify(params, null, 2));
-  docClient.put(params, function(err, data) {
-    if (err) {
-      logger.error("Unable to PUT item. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-      logger.debug("putItem succeeded:", JSON.stringify(data, null, 2));
-    }
-  });
-} 
-
-async function deleteItem(params) {
-  var docClient = await createAwsDynamoDBObject();
-
-  logger.debug("Deleting from table: " + params.TableName);
-  logger.debug(JSON.stringify(params, null, 2));
-  docClient.delete(params, function(err, data) {
-    if (err) {
-      logger.error("Unable to DELETE item. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-      logger.debug("deleteItem succeeded:", JSON.stringify(data, null, 2));
-    }
-  });
-}
+//   logger.debug("Getting from table: " + params.TableName);
+//   logger.debug(JSON.stringify(params, null, 2));
+//   docClient.get(params, function(err, data) {
+//     if (err) {
+//       logger.error("Unable to GET item. Error JSON:", JSON.stringify(err, null, 2));
+//     } else {
+//       logger.debug("getItem succeeded:", JSON.stringify(data, null, 2));
+//       return data;
+//     }
+//   });
+// } 
 
 
-async function createAwsDynamoDBObject() {
-  var AWS = require("aws-sdk");
-  console.log("AWS SDK Version: " + AWS.VERSION);
+// async function putItem(params) {
+//   var docClient = await createAwsDynamoDBObject();
 
-  const awsDynamoDbEndpoint = "https://dynamodb." + process.env.AWS_REGION + ".amazonaws.com";
-  logger.debug("Setting endpoint: " + awsDynamoDbEndpoint);
-  // var awsConfig = new AWS.Config();
-  AWS.config.update({endpoint: awsDynamoDbEndpoint, region: process.env.AWS_REGION});
-  AWS.config.apiVersions = { dynamodb: '2012-08-10' };
+//   logger.debug("Putting into table: " + params.TableName);
+//   logger.debug(JSON.stringify(params, null, 2));
+//   docClient.put(params, function(err, data) {
+//     if (err) {
+//       logger.error("Unable to PUT item. Error JSON:", JSON.stringify(err, null, 2));
+//     } else {
+//       logger.debug("putItem succeeded:", JSON.stringify(data, null, 2));
+//     }
+//   });
+// } 
 
-  // var dynamodb = new AWS.DynamoDB();
-  var docClient = new AWS.DynamoDB.DocumentClient();
+// async function deleteItem(params) {
+//   var docClient = await createAwsDynamoDBObject();
 
-  return docClient;
-}
+//   logger.debug("Deleting from table: " + params.TableName);
+//   logger.debug(JSON.stringify(params, null, 2));
+//   docClient.delete(params, function(err, data) {
+//     if (err) {
+//       logger.error("Unable to DELETE item. Error JSON:", JSON.stringify(err, null, 2));
+//     } else {
+//       logger.debug("deleteItem succeeded:", JSON.stringify(data, null, 2));
+//     }
+//   });
+// }
+
+
+// async function createAwsDynamoDBObject() {
+//   var AWS = require("aws-sdk");
+//   console.log("AWS SDK Version: " + AWS.VERSION);
+
+//   const awsDynamoDbEndpoint = "https://dynamodb." + process.env.AWS_REGION + ".amazonaws.com";
+//   logger.debug("Setting endpoint: " + awsDynamoDbEndpoint);
+//   // var awsConfig = new AWS.Config();
+//   AWS.config.update({endpoint: awsDynamoDbEndpoint, region: process.env.AWS_REGION});
+//   AWS.config.apiVersions = { dynamodb: '2012-08-10' };
+
+//   // var dynamodb = new AWS.DynamoDB();
+//   var docClient = new AWS.DynamoDB.DocumentClient();
+
+//   return docClient;
+// }
