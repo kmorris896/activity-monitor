e03337c5b6aa2d2f0028178a16162c7ff6f6a6cb
Kelly Morris <70837349+kmorris896@users.noreply.github.com>
2021-05-29 21:23:25 +0000

    added DynamoDB instructions

--- commands/channel_activity.js
+++ commands/channel_activity.js
@@ -1,16 +1,9 @@
-// ---------- DynamoDB Declarations
-var AWS = require("aws-sdk");
-const awsDynamoDbEndpoint = "https://dynamodb." + process.env.AWS_REGION + ".amazonaws.com";
-console.log("Setting endpoint: " + awsDynamoDbEndpoint);
-
-AWS.config.update({endpoint: awsDynamoDbEndpoint, region: process.env.AWS_REGION});
-AWS.config.apiVersions = { dynamodb: '2012-08-10' };
-var docClient = new AWS.DynamoDB.DocumentClient();
+const chatTable = "chatTable_" + process.env.DYNAMODB_TABLE_IDENTIFIER;
 
 module.exports = {
   name: 'channel_activity',
   description: 'Watch channel activity',
-  execute(logger, msg) {
+  execute(msg, logger, docClient) {
     const messageWordCount = msg.content.trim().split(/\s+/).length;
     
     msg.channel.messages.fetch({ limit: 5 , before: msg.id }).then(function (messages) {
@@ -18,6 +11,21 @@ module.exports = {
       return nonbot_messages.first().createdTimestamp;
     }).then(function (lastChannelMessageDatetime) {
       const lastChannelMessageDelta = msg.createdTimestamp - lastChannelMessageDatetime;
+
+      let chatItem = {
+        TableName: chatTable,
+        Item: {
+          "serverId": msg.guild.id,
+          "memberId": msg.author.id,
+          "channelId": msg.channel.id,
+          "messageWordCount": messageWordCount,
+          "messageSnowflake": msg.id,
+          "messageLink": msg.url,
+          "messageDateTime": msg.createdTimestamp,
+          "lastChannelMessageDelta": lastChannelMessageDelta
+        }
+      }
+      
       logger.debug(`serverId: ${msg.guild.id}`);
       logger.debug(`memberId: ${msg.author.id}`);
       logger.debug(`channelId: ${msg.channel.id}`);
@@ -26,7 +34,15 @@ module.exports = {
       logger.debug(`messageLink: ${msg.url}`);
       logger.debug(`messageDateTime: ${msg.createdTimestamp}`);
       logger.debug(`lastChannelMessageDelta: ${lastChannelMessageDelta}`);
-    })
+
+      docClient.put(chatItem, function(err, data) {
+        if (err) {
+          logger.error("Unable to PUT chat item.  Error JSON: " + JSON.stringify(err, null, 2));
+        } else {
+          logger.debug("chatItem putItem succeeded!");
+        }
+      });
+    });
 
   }
 }
--- index.js
+++ index.js
@@ -81,7 +81,7 @@ client.on('message', message => {
 
   } else if ((message.channel.id == "752154612798062612") || (message.channel.id == "752462096104423536") 
            ||(message.channel.id == "740177216134053890")) {
-    client.commands.get('channel_activity').execute(logger, message);
+    client.commands.get('channel_activity').execute(message, logger, docClient);
   } else {
     logger.debug(message.content);
   }
