4a66356193366faeeab6209c4e7ffb0cb6951984
Kelly Morris <70837349+kmorris896@users.noreply.github.com>
2021-05-31 13:47:44 +0000

    Moving dynamo functions to their own library.

--- commands/config.js
+++ commands/config.js
@@ -2,20 +2,21 @@
 const configTable = "configTable_" + process.env.DYNAMODB_TABLE_IDENTIFIER;
 
 // ---------- DynamoDB Declarations
-var AWS = require("aws-sdk");
-const awsDynamoDbEndpoint = "https://dynamodb." + process.env.AWS_REGION + ".amazonaws.com";
-console.log("Setting endpoint: " + awsDynamoDbEndpoint);
+var docClient = require("../lib/dynamoDB.js");
+// var AWS = require("aws-sdk");
+// const awsDynamoDbEndpoint = "https://dynamodb." + process.env.AWS_REGION + ".amazonaws.com";
+// console.log("Setting endpoint: " + awsDynamoDbEndpoint);
 
-AWS.config.update({endpoint: awsDynamoDbEndpoint, region: process.env.AWS_REGION});
-AWS.config.apiVersions = { dynamodb: '2012-08-10' };
-var docClient = new AWS.DynamoDB.DocumentClient();
+// AWS.config.update({endpoint: awsDynamoDbEndpoint, region: process.env.AWS_REGION});
+// AWS.config.apiVersions = { dynamodb: '2012-08-10' };
+// var docClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports = {
   name: 'config',
   description: 'Configure the bot',
   execute(msg, args) {
     if(msg.member.hasPermission('ADMINISTRATOR')) {
-      if (args[0] == "hasrole") {
+      if (args[0].toLowerCase() == "hasrole") {
         const hasRole = msg.mentions.roles.first();
   
         if (typeof hasRole != "undefined") {
@@ -27,15 +28,16 @@ module.exports = {
             }
           }
           
-          docClient.put(configItem, function(err, data) {
-            if (err) {
-              console.error("Unable to PUT item. Error JSON:" + JSON.stringify(err, null, 2));
-            } else {
-              console.debug("putItem succeeded:" + JSON.stringify(data, null, 2));
-              msg.client.botConfig[msg.guild.id].set("hasRole", hasRole.id);
-              return msg.channel.send("Bot will kick users with the role <@&" + hasRole.id + "> within the timeframe.");
-            }
-          });
+          docClient.putItem(configItem, msg.client.logger);
+          // docClient.put(configItem, function(err, data) {
+          //   if (err) {
+          //     console.error("Unable to PUT item. Error JSON:" + JSON.stringify(err, null, 2));
+          //   } else {
+          //     console.debug("putItem succeeded:" + JSON.stringify(data, null, 2));
+          //     msg.client.botConfig[msg.guild.id].set("hasRole", hasRole.id);
+          //     return msg.channel.send("Bot will kick users with the role <@&" + hasRole.id + "> within the timeframe.");
+          //   }
+          // });
         } else {
           if (msg.client.botConfig[msg.guild.id].has("hasRole")) {
             return msg.channel.send("Bot has been configured to kick users with the role <@&" + msg.client.botConfig[msg.guild.id].get("hasRole") + ">");
