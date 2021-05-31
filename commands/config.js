94ba263b3c1d58bd36d587dc1bd963cdbc48b84b
Kelly Morris <70837349+kmorris896@users.noreply.github.com>
2021-05-31 13:08:04 +0000

    Config lock behind admin

--- commands/config.js
+++ commands/config.js
@@ -14,34 +14,38 @@ module.exports = {
   name: 'config',
   description: 'Configure the bot',
   execute(msg, args) {
-    if (args[0] == "hasrole") {
-      const hasRole = msg.mentions.roles.first();
-
-      if (typeof hasRole != "undefined") {
-        const configItem = {
-          TableName: configTable,
-          Item: {
-            "serverId": msg.guild.id,
-            "hasRole": hasRole.id
+    if(msg.member.hasPermission('ADMINISTRATOR')) {
+      if (args[0] == "hasrole") {
+        const hasRole = msg.mentions.roles.first();
+  
+        if (typeof hasRole != "undefined") {
+          const configItem = {
+            TableName: configTable,
+            Item: {
+              "serverId": msg.guild.id,
+              "hasRole": hasRole.id
+            }
           }
-        }
-        
-        docClient.put(configItem, function(err, data) {
-          if (err) {
-            console.error("Unable to PUT item. Error JSON:" + JSON.stringify(err, null, 2));
-          } else {
-            console.debug("putItem succeeded:" + JSON.stringify(data, null, 2));
-            msg.client.botConfig[msg.guild.id].set("hasRole", hasRole.id);
-            return msg.channel.send("Bot will kick users with the role <@&" + hasRole.id + "> within the timeframe.");
-          }
-        });
-      } else {
-        if (msg.client.botConfig[msg.guild.id].has("hasRole")) {
-          return msg.channel.send("Bot has been configured to kick users with the role <@&" + msg.client.botConfig[msg.guild.id].get("hasRole") + ">");
+          
+          docClient.put(configItem, function(err, data) {
+            if (err) {
+              console.error("Unable to PUT item. Error JSON:" + JSON.stringify(err, null, 2));
+            } else {
+              console.debug("putItem succeeded:" + JSON.stringify(data, null, 2));
+              msg.client.botConfig[msg.guild.id].set("hasRole", hasRole.id);
+              return msg.channel.send("Bot will kick users with the role <@&" + hasRole.id + "> within the timeframe.");
+            }
+          });
         } else {
-          return msg.channel.send("Bot has not been configured to look for a role.");
-        } 
+          if (msg.client.botConfig[msg.guild.id].has("hasRole")) {
+            return msg.channel.send("Bot has been configured to kick users with the role <@&" + msg.client.botConfig[msg.guild.id].get("hasRole") + ">");
+          } else {
+            return msg.channel.send("Bot has not been configured to look for a role.");
+          } 
+        }
       }
+    } else {
+      msg.reply('You must must be an administrator in order configure the bot.')
     }
   },
   getServerConfig(serverId, client, logger, docClient) {
