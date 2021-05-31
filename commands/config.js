d9bb98e854edb72aa7e8af27dc2b9f512c3830c2
Kelly Morris <70837349+kmorris896@users.noreply.github.com>
2021-05-31 16:23:28 +0000

    More config conversion.

--- commands/config.js
+++ commands/config.js
@@ -1,5 +1,6 @@
 // ---------- DynamoDB Configuration
 const configTable = "configTable_" + process.env.DYNAMODB_TABLE_IDENTIFIER;
+const docClient = null;
 
 module.exports = {
   name: 'config',
@@ -7,27 +8,22 @@ module.exports = {
   async execute(msg, args) {
     if(msg.member.hasPermission('ADMINISTRATOR')) {
       if (args[0].toLowerCase() == "hasrole") {
+        msg.client.logger.debug("config hasrole");
         const hasRole = msg.mentions.roles.first();
   
         if (typeof hasRole != "undefined") {
-          const configItem = {
-            TableName: configTable,
-            Item: {
-              "serverId": msg.guild.id,
-              "hasRole": hasRole.id
-            }
-          }
-
           try {
-            await msg.client.dynamoClient.put(configItem);
-            msg.client.botConfig[msg.guild.id].config.set("hasRole", hasRole.id);
-            return msg.channel.send("Bot will kick users with the role <@&" + hasRole.id + "> within the time horizon.");
+            msg.client.logger.debug("DEBUG config: " + JSON.stringify(msg.client.botConfig[msg.guild.id].get("config").hasRole));
+            msg.client.logger.debug(typeof msg.client.botConfig[msg.guild.id].get("config"));
+            msg.client.botConfig[msg.guild.id].get("config").hasRole = hasRole.id;
+            await saveConfig(msg.guild.id, msg.client);
+            return msg.channel.send("Bot will kick users with the role <@&" + msg.client.botConfig[msg.guild.id].get("config").hasRole + "> within the time horizon.");
           } catch(err) {
-            msg.client.logger.error(JSON.stringify(err, null, 2));
+            msg.client.logger.error("config hasrole - unable to store config item into database: " + JSON.stringify(err, null, 2));
             return msg.channel.send("Bot was not able to store the config item into the database.  Please contact the bot developer.");
           }          
         } else {
-          if (msg.client.botConfig[msg.guild.id].config.has("hasRole")) {
+          if (msg.client.botConfig[msg.guild.id].config.hasOwnProperty("hasRole")) {
             return msg.channel.send("Bot has been configured to kick users with the role <@&" + msg.client.botConfig[msg.guild.id].get("hasRole") + ">");
           } else {
             return msg.channel.send("Bot has not been configured to look for a role.");
@@ -38,7 +34,8 @@ module.exports = {
       msg.reply('You must must be an administrator in order configure the bot.')
     }
   },
-  async getServerConfig(serverId, client, logger) {
+
+  async getServerConfig(serverId, client) {
     const configParams = {
       TableName: configTable,
       Key: {
@@ -48,20 +45,25 @@ module.exports = {
   
     try {
       const data = await client.dynamoClient.get(configParams).promise();
+
       if (data.hasOwnProperty("Item") && data.Item.hasOwnProperty("hasRole")) {
-        logger.debug("DEPRECATION ALERT: getServerConfig hasRole detected.  Converting to config object");
-        client.botConfig[serverId].set("config", convertToConfigObject(serverId, data.Item.hasRole, logger));
+        client.logger.debug("DEPRECATION ALERT: getServerConfig hasRole detected.  Converting to config object");
+        client.botConfig[serverId].set("config", convertToConfigObject(serverId, data.Item.hasRole, client));
+      } else if (data.hasOwnProperty("Item") && data.Item.hasOwnProperty("config")) {
+        client.logger.debug("Setting server Config to DynamoDB stored values.");
+        client.botConfig[serverId].set("config", data.Item.config);
+        client.logger.debug("config: " + JSON.stringify(client.botConfig[serverId].get("config")));
       } else {
-        logger.debug("getItem returned empty");
+        client.logger.debug("getItem returned empty");
       }    
     } catch(err) {
-      logger.error("getServerConfig - Unable to read item.  Error JSON: " + JSON.stringify(err, null, 2));
+      client.logger.error("getServerConfig - Unable to read item.  Error JSON: " + JSON.stringify(err, null, 2));
     };
 
   }
 }
 
-function convertToConfigObject(serverId, hasRole_IN, logger) {
+async function convertToConfigObject(serverId, hasRole_IN, logger) {
   const configObject = {
     "hasRole": hasRole_IN,
     "checkInterval": "1h",
@@ -70,24 +72,34 @@ function convertToConfigObject(serverId, hasRole_IN, logger) {
     "kickAnnouncementChannel": ""
   };
 
+  client.botConfig[serverId].set("config", configObject);
+
   try{
-    saveConfig(serverId, configObject);
+    await saveConfig(serverId, client);
   } catch(err) {
     logger.error("Unable able to save config after converting hasRole to Config Object.");
     logger.error(JSON.stringify(err, null, 2));
   }
 
-  return configObject; 
+  return configMap; 
 }
 
-function saveConfig(serverId, configObject) {
+async function saveConfig(serverId, client) {
+  client.logger.debug("Saving Config for " + serverId)
   const configItem = {
     TableName: configTable,
     Item: {
       "serverId": serverId,
-      "config": configObject
+      "config": client.botConfig[serverId].get("config")
     }
   }
   
-  docClient.putItem(configItem);
+  client.logger.debug(JSON.stringify(configItem));
+  try {
+    const results = await client.dynamoClient.put(configItem).promise();
+    return results;
+  } catch (err) {
+    client.logger.error("config.js - saveConfig - Error saving config: " + JSON.stringify(err, null, 2));
+  }
+  
 }
